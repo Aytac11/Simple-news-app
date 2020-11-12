fdb914cff5bce68d00948958382989438fbd026f
Aytac11 <69673205+Aytac11@users.noreply.github.com>
2020-11-12 12:28:46 +0400

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,5 @@ def xeber():
 def SingleNews(id):
      return render_template('single.html', news_id=id, data=news)
 
-# @app.route("/add", methods =["get","post"])
-# def add():
-#     if request.method=="post":
-#         news_title=request.form['title']
-#         news_detail=request.form['detail']
-#     return render_template('add_news.html',title=news_title,detail=news_detail)
 if __name__ == "__main__":
     app.run(debug=True)
